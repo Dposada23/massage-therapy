352cdc56004adf4503dc1e6c3bc52dc9073a0b4a
Dposada23 <daposada1000@gmail.com>
2021-07-27 00:51:44 -0500

    added firebase hosing + contdown timer

--- massage-thp/src/app/home/home.component.ts
+++ massage-thp/src/app/home/home.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription!: Subscription;
 
-  ngOnInit(): void {
+  public dateNow = new Date();
+  public dDay = new Date('July 31 2021 13:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+
+  public timeDifference: number | undefined;
+  public secondsToDday: number | undefined;
+  public minutesToDday: number | undefined;
+  public hoursToDday: number | undefined;
+  public daysToDday: number | undefined;
+
+
+  private getTimeDifference () {
+      this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+      this.allocateTimeUnits(this.timeDifference);
   }
 
+private allocateTimeUnits (timeDifference: any) {
+      this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+      this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+      this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+      this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
 }
+
+  ngOnInit() {
+     this.subscription = interval(1000)
+         .subscribe(x => { this.getTimeDifference(); });
+  }
+
+ ngOnDestroy() {
+    this.subscription.unsubscribe();
+ }
+
+}
+
